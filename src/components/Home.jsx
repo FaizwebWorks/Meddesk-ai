8ff87de7a2db06dd1f67e8061e2f566ea0997394
FaizwebWorks <webWorksbyfaiz@gmail.com>
2024-09-01 21:12:50 +0530

    Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,29 +1,39 @@
 const Home = () => {
   return (
     <>
-      <div className="w-full flex items-center gap-16 justify-center px-10 h-[70vh] mt-4">
-        <button className="h-40 btn w-40 flex items-center justify-center font-bold text-zinc-900">
-          Push button
-        </button>
-        <button className="h-40 btn w-40 flex items-center justify-center font-bold text-zinc-900">
-          Push button
-        </button>
-        <button className="h-40 btn w-40 flex items-center justify-center font-bold text-zinc-900">
-          Push button
-        </button>
-        <button className="h-40 btn w-40 flex items-center justify-center font-bold text-zinc-900">
-          Push button
-        </button>
-        {/* <button className="h-32 w-32 flex items-center justify-center rounded-full bg-green-500 font-semibold text-white">
-          Push button
-        </button>
-        <button className="h-32 w-32 flex items-center justify-center rounded-full bg-green-500 font-semibold text-white">
-          Push button
-        </button>
-        <button className="h-32 w-32 flex items-center justify-center rounded-full bg-green-500 font-semibold text-white">
-          Push button
-        </button> */}
+      <div className="w-full flex items-center gap-16 justify-center px-10 h-[58vh] mt-4">
+        <div>
+          <button className="h-44 btn w-44 flex items-center justify-center font-bold text-zinc-900">
+            Push button
+          </button>
+          <h1 className="text-2xl ml-[46px] mt-6 text-black uppercase font-medium">Topic 1</h1>
+        </div>
+        <div>
+          <button className="h-44 btn w-44 flex items-center justify-center font-bold text-zinc-900">
+            Push button
+          </button>
+          <h1 className="text-2xl ml-[46px] mt-6 text-black uppercase font-medium">Topic 2</h1>
+        </div>
+        <div>
+          <button className="h-44 btn w-44 flex items-center justify-center font-bold text-zinc-900">
+            Push button
+          </button>
+          <h1 className="text-2xl ml-[46px] mt-6 text-black uppercase font-medium">Topic 3</h1>
+        </div>
+        <div>
+          <button className="h-44 btn w-44 flex items-center justify-center font-bold text-zinc-900">
+            Push button
+          </button>
+          <h1 className="text-2xl ml-[46px] mt-6 text-black uppercase font-medium">Topic 4</h1>
+        </div>
+        
       </div>
+      {/* <div className="flex items-center w-full justify-center gap-[150px] px-10 text-black">
+        <h1 className="text-2xl uppercase font-medium">Topic 1</h1>
+        <h1 className="text-2xl uppercase font-medium">Topic 2</h1>
+        <h1 className="text-2xl uppercase font-medium">Topic 3</h1>
+        <h1 className="text-2xl uppercase font-medium">Topic 4</h1>
+      </div> */}
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import logo from "../assets/meddesk-logo.png"
 const Navbar = () => {
   return (
     <div className="w-full relative py-4 px-10 flex items-center justify-between">
-        <img width={120} src={logo} alt="LOGO" />
-        <p className="absolute text-sm font-medium text-[#0a6382] mt-9">IT just works Better</p>
+        <img width={250} className="bg-white p-1 mt-[-40px]" src={logo} alt="LOGO" />
+        <p className="absolute text-lg font-medium text-black bg-white uppercase mt-10 w-fit px-[29px]">IT just works Better</p>
         <div className="flex items-center gap-6 font-medium pt-2">
-            <a href="/">Home</a>
-            <a href="/transcript">Transcript</a>
-            <a href="/report">Report</a>
-            <a href="/p3">p3</a>
+            <a className="border-2 border-green-500 p-8 text-black text-lg bg-pink-200" href="/">Home</a>
+            <a className="bg-white border-2 border-green-500 p-8 text-black text-lg" href="/transcript">Transcript</a>
+            <a className="bg-white border-2 border-green-500 p-8 text-black text-lg" href="/report">Report</a>
+            <a className="bg-white border-2 border-green-500 p-8 text-black text-lg" href="/p3">p3</a>
         </div>
     </div>
   )
